20d16fc837ad3bf19534276d8a3cf32b5ade9f82
Pablo_3_16 <ergle.pasha@gmail.com>
2025-04-09 17:09:48 +0300

    implement start server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+	"project9/internal/server"
+	"time"
+)
+
 func main() {
 
+	if len(os.Args) != 3 {
+		panic("допустимо только 3 аргумента: <программа> <хост> <dsn>")
+	}
+
+	adrs := os.Args[1]
+	dsn := os.Args[2]
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		panic(fmt.Errorf("ошибка открытия БД: %w", err))
+	}
+	defer db.Close()
+
+	pingCtx, cancel := context.WithTimeout(context.Background(), time.Second*7)
+	defer cancel()
+
+	if err = db.PingContext(pingCtx); err != nil {
+		panic(fmt.Errorf("ошибка подключения к БД: %w", err))
+	}
+
+	server.Start(adrs)
 }
--- internal/server/start_server.go
+++ internal/server/start_server.go
@@ -2,26 +2,58 @@ package server
 
 import (
 	"context"
-	"database/sql"
+	"errors"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-func Start(adrs string, dns string) {
+func Start(adrs string) {
 
-	db, err := sql.Open("pgx", dns)
-	if err != nil {
-		panic(fmt.Errorf("ошибка открытия БД: %w", err))
+	var shutdownTimeout time.Duration
+	var err error
+
+	shutdownTimeoutStr, exists := os.LookupEnv("SHUTDOWN_TIMEOUT")
+	if exists {
+		shutdownTimeout, err = time.ParseDuration(shutdownTimeoutStr)
+		if err != nil {
+			fmt.Printf("ошибка валидации SHUTDOWN_TIMEOUT (установелено значение по умолчанию - 7s): %v\n", err)
+			shutdownTimeout = 7 * time.Second
+		}
+	} else {
+		shutdownTimeout = 7 * time.Second
 	}
-	defer db.Close()
 
-	pingCtx, cancel := context.WithTimeout(context.Background(), time.Second*7)
-	defer cancel()
+	serverCtx, serverCancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer serverCancel()
 
-	if err = db.PingContext(pingCtx); err != nil {
-		panic(fmt.Errorf("ошибка подключения к БД: %w", err))
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		Addr:    adrs,
+		Handler: mux,
 	}
+
+	log.Printf("Сервер запущен. Адрес: %s. PID: %d\n", adrs, os.Getppid())
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal(err)
+		}
+	}()
+
+	<-serverCtx.Done()
+	ctxTimeout, cancelTimeout := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancelTimeout()
+
+	server.Shutdown(ctxTimeout)
+	log.Println("Сервер закрыт")
 
 }
