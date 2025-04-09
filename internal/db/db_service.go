79f5c64aea951cb7b49f4192404221c036deee17
Pablo_3_16 <ergle.pasha@gmail.com>
2025-04-09 19:12:00 +0300

    implement create & not by id not

--- internal/db/db_service.go
+++ internal/db/db_service.go
@@ -16,6 +16,7 @@ func CreateNoteTable() error {
 	defer cancel()
 
 	db := sqlx.MustConnect("pgx", dsn)
+	defer db.Close()
 
 	notesTable := `CREATE TABLE IF NOT EXISTS notes (
 		id SERIAL PRIMARY KEY,
@@ -26,4 +27,38 @@ func CreateNoteTable() error {
 	_, err := db.ExecContext(ctx, notesTable)
 
 	return err
+}
+
+func CreateNote(title string, content string) error {
+
+	query := `INSERT INTO notes (title, content) VALUES ($1, $2);`
+
+	dsn := os.Getenv("DSN")
+	ctx, cancel := context.WithTimeout(context.Background(), 7*time.Second)
+	defer cancel()
+	db := sqlx.MustConnect("pgx", dsn)
+	defer db.Close()
+
+	_, err := db.QueryContext(ctx, query, title, content)
+
+	return err
+}
+
+func GetNoteById(id string) (Note, error) {
+
+	var note Note
+	query := `SELECT * FROM notes WHERE id = $1`
+
+	dsn := os.Getenv("DSN")
+	ctx, cancel := context.WithTimeout(context.Background(), 7*time.Second)
+	defer cancel()
+	db := sqlx.MustConnect("pgx", dsn)
+	defer db.Close()
+
+	err := db.GetContext(ctx, &note, query, id)
+	if err != nil {
+		return note, err
+	}
+
+	return note, nil
 }
